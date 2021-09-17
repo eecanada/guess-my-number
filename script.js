b5e868a16964acff23e639db0e1893f38ea76935
Eder Canada <canada.eder@gmail.com>
2021-09-16 18:39:35 -0700

    reset function

--- script.js
+++ script.js
@@ -11,10 +11,11 @@
 
 // document.querySelector('.guess').value = 23
 
-const secretNumber = Math.trunc(Math.random() * 20 + 1);
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
+
 document.querySelector('.number').textContent = secretNumber;
 
-let score = (document.querySelector('.score').textContent = 5);
+let score = (document.querySelector('.score').textContent = 20);
 
 document.querySelector('.check').addEventListener('click', function () {
   let guess = Number(document.querySelector('.guess').value);
@@ -23,6 +24,12 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = ' ⛔️ No Number!';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = ' 🎉 Correct Number!';
+
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.body.style.background = '#2BC20E';
+
+    document.querySelector('.number').style.width = '30rem';
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = ' 📈 Too High!';
@@ -43,6 +50,23 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = '💥 GAME OVER!';
       document.querySelector('.score').textContent = 0;
     }
-    
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  if (score == 20) {
+    document.querySelector('.message').textContent =
+      'Cannot reset, start guessing!';
+  } else {
+    score = 20;
+    document.querySelector('.score').textContent = 20;
+    secretNumber = Math.trunc(Math.random() * 20 + 1);
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.body.style.background = '#222';
+    document.querySelector('.number').style.width = '15rem';
+    document.querySelector('.message').textContent = 'Start guessing...';
+
+    document.querySelector('.guess').value = ""
   }
 });
