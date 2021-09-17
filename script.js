2cf6059ca3072e56f517a4d1b6651e179657e7ba
Eder Canada <canada.eder@gmail.com>
2021-09-16 17:22:20 -0700

    if no response

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.message').textContent = ' 🎉 Correct Number!';
+// document.querySelector('.message').textContent = ' 🎉 Correct Number!';
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
 
-document.querySelector('.guess').value = 23 
+// document.querySelector('.guess').value = 23 
+
+function eder(){
+  document.querySelector('.guess').value = 23
+}
+
+function check(){
+  document.querySelector('.message').textContent = ' 🎉 Correct Number!';
+
+}
+
+document.querySelector('.check').addEventListener('click', function (){
+  let guess = document.querySelector('.guess').value 
+
+  if(!guess){
+    document.querySelector('.message').textContent = ' ⛔️ No Number!';
+  } else {
+    document.querySelector('.message').textContent =  guess ;
+  }
+})
